bb4729696638a4d3c859f9de9952954f6427dec3
Noah Oppenheim <noah17@bu.edu>
2023-07-09 15:32:05 +0300

    Improved the file reading function and proccessing

--- core_app.py
+++ core_app.py
@@ -1,50 +1,53 @@
-from PyPDF2 import PdfReader
+import re
+import fitz
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 import os
 
+# Processes text to make it easier to read.
+def preprocess(text):
+    text = text.replace('\n', ' ')
+    text = re.sub('\s+', ' ', text)
+    return text
+
 # Location of the PDF file
-file_path = "/Users/noah/Downloads/992-998 Amsterdam Avenue Offering Memorandum.pdf"
+file_path = "/Users/noah/Documents/Atlas invest documents/NYC Finance - Printable Page.pdf"
 
-# Read the PDF file using PdfReader
-doc_reader = PdfReader(file_path)
+# Changes the PDF to readable text
+def pdf_to_text(path, start_page=1, end_page=None):
+    doc = fitz.open(path)
+    total_pages = doc.page_count
 
-# Extract text from each page of the PDF
-raw_text = ""
-for page in doc_reader.pages:
-    text = page.extract_text()
-    if text:
-        raw_text += text
+    if end_page is None:
+        end_page = total_pages
 
-# Splitting up the text into smaller chunks for indexing
-text_splitter = CharacterTextSplitter(
-    separator="\n",
-    chunk_size=400,
-    chunk_overlap=70,
-    length_function=len
-)
+    text_list = []
 
+    for i in range(start_page - 1, end_page):
+        page = doc.load_page(i)
+        text = page.get_text("text")
+        text = preprocess(text)
+        text_list.append(text)
 
-
-texts = text_splitter.split_text(raw_text)
+    doc.close()
+    return text_list
 
 # Download embeddings from OpenAI
 embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
 
+# Read the PDF file and convert it to text
+texts = pdf_to_text(file_path)
+
 # Create a vector store using FAISS
 doc_search = FAISS.from_texts(texts, embeddings)
 
-
 # Test question
-query = "what is the LTC?"
+query = "Are there any late payments, or payments still due that should have been payed? The date is 07/06/2023 American date"
 
 # Perform similarity search
-docs = doc_search.similarity_search(query, k=20)
-
-
+docs = doc_search.similarity_search(query, k=5)
 
 # Load the question-answering chain
 chain = load_qa_chain(OpenAI(), chain_type="stuff")
